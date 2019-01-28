fe7cc6ec7e6e51f4702ab49c21d082f5e24c95aa
buffalorob <peckham.rob@gmail.com>
2019-01-28 15:22:19 -0500

    migrated after creating logs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_200203) do
+ActiveRecord::Schema.define(version: 2019_01_28_201904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,20 @@ ActiveRecord::Schema.define(version: 2019_01_28_200203) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "logs", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "category_id"
+    t.string "notes"
+    t.string "tools"
+    t.integer "cost"
+    t.datetime "date_performed"
+    t.datetime "date_due"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_logs_on_category_id"
+    t.index ["item_id"], name: "index_logs_on_item_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "login"
     t.string "name"
@@ -40,4 +54,6 @@ ActiveRecord::Schema.define(version: 2019_01_28_200203) do
 
   add_foreign_key "categories", "items"
   add_foreign_key "items", "users"
+  add_foreign_key "logs", "categories"
+  add_foreign_key "logs", "items"
 end
