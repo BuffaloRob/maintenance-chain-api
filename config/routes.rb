f6f58fb9e74276ee8acec98ec2ff07cf1bf9bcfc
buffalorob <peckham.rob@gmail.com>
2019-01-28 15:21:57 -0500

    routes: + logs to api::v1 namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :logs
   namespace :api do
     namespace :v1 do
       resources :users
       resources :items
       resources :categories
+      resources :logs
     end
   end  
 end      
