c9c9f88ab072e764b84cae3133c9d8f2465dc86a
buffalorob <peckham.rob@gmail.com>
2019-01-28 15:02:57 -0500

    updated schema via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_192311) do
+ActiveRecord::Schema.define(version: 2019_01_28_200203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.bigint "item_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_categories_on_item_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.bigint "user_id"
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 2019_01_28_192311) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "categories", "items"
   add_foreign_key "items", "users"
 end
