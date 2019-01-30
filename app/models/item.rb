5af09b2e13c3ba7f2323e8b4ee09c7e33975223f
buffalorob <peckham.rob@gmail.com>
2019-01-30 11:51:42 -0500

    deleted user and item scaffolds

--- app/models/item.rb
+++
@@ -1,7 +0,0 @@
-class Item < ApplicationRecord
-  belongs_to :user
-  has_many :logs, dependent: :destroy
-  has_many :categories, dependent: :destroy
-
-  validates :name, presence: true, uniqueness: { scope: :user_id }
-end
